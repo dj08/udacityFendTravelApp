6167667959ebe112bc1c2bada21be222fcee20c3
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-27 11:25:19 -0500

    Add beta webpack config and new packages

---
+++ webpack.dev.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    stats: {
+	logging: 'verbose',
+	colors: true,
+    },
+    entry: './src/index.js',
+    output: {
+	libraryTarget: 'var',
+	library: 'Client'
+    },
+    module: {
+	rules: [
+	    {
+		// Run all custom JS in project through
+		// babel. Otherwise webpack will move advanced ES
+		// syntaxes to browser distribution and will create
+		// issues there.
+		test: /\.js$/,
+		exclude: /node_modules/,
+		loader: "babel-loader"
+	    },
+	    {
+		enforce: 'pre', // This comes before babel-loader
+		test: /.js$/,
+		exclude: /node_modules/,
+		loader: 'eslint-loader',
+		options: {}
+	    },
+	    {
+		test: /\.scss$/,
+		use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+	    }
+	]
+    },
+    plugins: [
+	new HtmlWebPackPlugin({
+	    template: './src/views/index.html',
+	    filename: './index.html'
+	}),
+	new CleanWebpackPlugin({
+	    dry: true,
+	    verbose: true,
+	    cleanStaleWebpackAssets: true,
+	    protectWebpackAssets: false
+	}),
+	new BundleAnalyzerPlugin(),
+    ],
+};
