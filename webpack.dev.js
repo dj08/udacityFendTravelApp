855a2f35d5049c243f461cdf934f9d3e8b4a19ae
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-27 23:04:30 -0500

    Split dev config to common and dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,36 +1,12 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
+module.exports = merge(common, {
     mode: 'development',
     devtool: 'source-map',
-    stats: {
-	logging: 'verbose',
-	colors: true,
-    },
-    entry: [
-	// This is needed for babel to understand async/await
-	'./node_modules/regenerator-runtime/runtime',
-	'./src/client/index.js'
-    ],
-    output: {
-	libraryTarget: 'var',
-	library: 'Client'
-    },
     module: {
 	rules: [
-	    {
-		// Run all custom JS in project through
-		// babel. Otherwise webpack will move advanced ES
-		// syntaxes to browser distribution and will create
-		// issues there.
-		test: /\.js$/,
-		exclude: /node_modules/,
-		loader: "babel-loader"
-	    },
 	    {
 		enforce: 'pre', // This comes before babel-loader
 		test: /.js$/,
@@ -45,16 +21,6 @@ module.exports = {
 	]
     },
     plugins: [
-	new HtmlWebPackPlugin({
-	    template: './src/client/views/index.html',
-	    filename: './index.html'
-	}),
-	new CleanWebpackPlugin({
-	    dry: true,
-	    verbose: true,
-	    cleanStaleWebpackAssets: true,
-	    protectWebpackAssets: false
-	}),
-	new BundleAnalyzerPlugin(),
+	new BundleAnalyzerPlugin()
     ],
 };
