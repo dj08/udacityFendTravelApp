3422fbe3308a97e3e75013277a12f917353cce2c
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-28 10:36:37 -0500

    Redirect all paths during dev via proxy server.

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,39 +10,34 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'source-map',
     devServer: {
-	// webpack-dev-server setup
-	host: 'localhost',
-	port: constants.WEBPACK_DEV_PORT,
-	proxy: {
-	    // The frontend code uses the backend to store
-	    // data. webpack-dev-server fails at this. Hence
-	    // redirecting frontend api requests to a different port.
-	    '/saveData' : {
-		target: backendServer,
-		secure: false
-	    },
-	    '/getData' : {
-		target: backendServer,
-		secure: false
-	    }
-	}
+        // webpack-dev-server setup
+        host: 'localhost',
+        port: constants.WEBPACK_DEV_PORT,
+        proxy: {
+            // The frontend code uses the backend to store
+            // data. webpack-dev-server fails at this. Hence
+            // redirecting frontend api requests to a different port.
+            context: () => true,
+            target: backendServer,
+            secure: false
+        }
     },
     module: {
-	rules: [
-	    {
-		enforce: 'pre', // This comes before babel-loader
-		test: /.js$/,
-		exclude: /node_modules/,
-		loader: 'eslint-loader',
-		options: {}
-	    },
-	    {
-		test: /\.scss$/,
-		use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-	    }
-	]
+        rules: [
+            {
+                enforce: 'pre', // This comes before babel-loader
+                test: /.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {}
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
     },
     plugins: [
-	new BundleAnalyzerPlugin()
+        new BundleAnalyzerPlugin()
     ],
 });
