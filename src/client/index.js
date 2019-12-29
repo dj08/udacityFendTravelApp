aa30cb92d9eba3d7dcc2df9d440727630bbfe36b
Dushyant Juneja <juneja.dushyant@gmail.com>
2019-12-29 12:37:58 -0500

    Add initial DOM interaction plan

--- src/client/index.js
+++ src/client/index.js
@@ -17,16 +17,32 @@ import './styles/style.scss';
 // to do this after the page has loaded, irrespective of how webpack
 // handles js insertion in our HTML view.
 document.addEventListener('DOMContentLoaded', () => {
-    const submitButton = document.getElementById('submit');
+    /* Here is how the app is expected to work:
+       1. At start: only city field is active. Date field is visibly inactive.
+       2. User enters the city. Reaction: 
+          - Weather div gets populated with request for dep date.
+          - Date field is now available.
+       3. User enters the date. Reaction:
+          - Coordinates query API fires off. It is slowest.
+          - Image search shows loading sign.
+	  - Image fetcher API fires and updates image.
+          - Weather div updates to show that it is fetching weather
+	  - Weather API fires off after ensuring that coordinates are
+       available.
+       4. All data is reset when city changes
+    */
+    const cityField = document.getElementById('city');
+    const dateField = document.getElementById('new-travel-date');
+    
     submitButton.addEventListener('click', Client.getLocationCoordinates);
 
-    const dateInput = document.getElementById('new-travel-date');
+    const dateInput = 
     dateInput.addEventListener('input', Client.getDateInput);
 });
+/* eslint-enable */
 
 export {
     getLocationCoordinates,
     getDateInput,
     getWeatherForecast
 };
-/* eslint-enable */
